1bc43f9503b3e991adba46229179346c2b4e0da6
Egor <144605759+dececus@users.noreply.github.com>
2023-10-08 19:41:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import discord
+import os
 import random
+import requests
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents = intents)
+bot = commands.Bot(command_prefix='!', intents = discord.Intents.default())
 
 @bot.event
 async def on_ready():
@@ -22,12 +24,35 @@ async def emoji(ctx):
 
 @bot.command()
 async def cat(ctx):
-    elements = ['cat1.jpg', 'cat2.jpg', 'cat3.jpg', 'cat4.jpg', 'cat5.jpg']
-    await ctx.send(file=discord.File(random.choice(elements)))
+    img_name = random.choice(os.listdir('cat'))
+    with open(f'cat/{img_name}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file=picture)
     
 @bot.command()
-async def meme(ctx):
-    elements = ['meme1.jpg', 'meme2.jpg', 'meme3.jpg', 'meme4.jpg', 'meme5.jpg']
-    await ctx.send(file=discord.File(random.choice(elements)))
+async def memec(ctx):
+    img_name = random.choice(os.listdir('meme_cat'))
+    with open(f'meme_cat/{img_name}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file=picture)
 
-bot.run("MTE1NTUwMDI1ODMzMjI1MDIyMg.GAlEGt.mpQvOkQ1ZEQEz5Ug5MeXGPMRKtG5mYmph6Bin4")
+@bot.command()
+async def memep(ctx):
+    img_name = random.choice(os.listdir('meme_prog'))
+    with open(f'meme_prog/{img_name}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+bot.run("MTE1NTUwMDI1ODMzMjI1MDIyMg.GDjndA.wZJ3VX7WQTfgHodIpg_Z9Ag1ZjOpu1MXKFpa20")
